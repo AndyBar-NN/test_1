09992503efaefea51f2713fdf787924860e6122f
AndyBar <andreysparrow91@rambler.ru>
2021-05-09 20:57:59 +0300

    change and add accardeon

--- js/main.js
+++ js/main.js
@@ -1,104 +1,132 @@
-const logoTitle = document.querySelector('.logo__title'),
-      slidebox = document.querySelector('.slidebox'),
-      slideboxBg = document.querySelector('.slidebox__slide'),
-      slideboxText = document.querySelector('.slidebox__text'),
-      logo = document.querySelector('.logo'),
-      brotherhood = document.querySelector('.brotherhood');
+document.addEventListener('DOMContentLoaded', () => {
+  const logoTitle = document.querySelector('.logo__title'),
+        slide = document.querySelectorAll('.slidebox__slide'),
+        slideboxText = document.querySelectorAll('.slidebox__text'),
+        map = document.querySelector('.map'),
+        gondor = document.querySelector('.gondor'),
+        shir = document.querySelector('.shir'),
+        rivendel = document.querySelector('.rivendel'),
+        karadras = document.querySelector('.karadras'),
+        moria = document.querySelector('.moria'),
+        lorien = document.querySelector('.lorien'),
+        anduin = document.querySelector('.anduin'),
+        logo = document.querySelector('.logo'),
+        brotherhood = document.querySelector('.brotherhood');
 
-slideboxBg.style.cssText = "background: url('img/map.jpg') no-repeat center;";
+  document.addEventListener('scroll', () => {
+    let scrollNum = 0.0025;
+    let scrollOp = 0.0010;
+    let scroll = window.pageYOffset;
 
-document.addEventListener('scroll', () => {
-  let scrollOp = 0.0025;
-  let scroll = window.pageYOffset;
+    if (scroll == 0) {
+      logo.style.opacity = 0.75;
+      map.style.opacity = 0.25;
+      logoTitle.style.transform = 'scale(1)';
+    }
+    if (scroll >= 100) {
+      logoTitle.style.transform = 'scale(1.5)';
+      logo.style.opacity = 0.75 - scroll * scrollNum;
+      map.style.opacity = 0.25 + scroll * scrollNum;
+    }
+    if (scroll >= 400) {
+      slideboxText[0].style.opacity = '0.85';
+      brotherhood.style.opacity = '1';
+    } else {
+      slideboxText[0].style.opacity = '';
+      brotherhood.style.opacity = '';
+    }
 
-  if (scroll == 0) {
-    logo.style.opacity = 0.75;
-    slidebox.style.opacity = 0.25;
-    logoTitle.style.transform = 'scale(1)';
-  }
-  if (scroll >= 100) {
-    logoTitle.style.transform = 'scale(1.5)';
-    logo.style.opacity = 0.75 - scroll * scrollOp;
-    slidebox.style.opacity = 0.25 + scroll * scrollOp;
-  }
-  if (scroll >= 500 && scroll < 600) {
-    slideboxText.style.opacity = '';
-  }
-  if (scroll >= 500) {
-    brotherhood.style.opacity = '1';
-  } else {
-    brotherhood.style.opacity = '';
-  }
+    slide.forEach((item) => {
+      if (scroll >= 500) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = '';
+      }
+    });
+    if (scroll >= 500) {
+      map.style.top = 0 - (scroll - 500) + 'px';
+      gondor.style.opacity = -0.5 + scroll * scrollOp;
+    } else {
+      map.style.top = '';
+    }
+    if (scroll >= 1500) {
+      slideboxText[1].style.opacity = '0.85';
+    } else {
+      slideboxText[1].style.opacity = '';
+    }
 
-  if (scroll >= 600) {
-    slideboxText.style.opacity = '1';
-    slideboxText.textContent = '1';
-  }
-  if (scroll >= 800) {
-    slideboxText.style.opacity = '';
-    slideboxBg.style.cssText = "background: url('img/gondor.jpg') no-repeat center;";
-  } else {
-    slideboxBg.style.cssText = "background: url('img/map.jpg') no-repeat center;";
-  }
+    if (scroll >= 1600) {
+      gondor.style.top = 0 - (scroll - 1600) + 'px';
+      shir.style.opacity = -1.6 + scroll * scrollOp;
+    } else {
+      gondor.style.top = '';
+    }
+    if (scroll >= 2600) {
+      slideboxText[2].style.opacity = '0.85';
+    } else {
+      slideboxText[2].style.opacity = '';
+    }
 
-  if (scroll >= 900) {
-    slideboxText.style.opacity = '1';
-    slideboxText.textContent = '2';
-  }
-  if (scroll >= 1100) {
-    slideboxText.style.opacity = '';
-    slideboxBg.style.cssText = "background: url('img/shir.jpg') no-repeat center;";
-  }
+    if (scroll >= 2700) {
+      shir.style.top = 0 - (scroll - 2700) + 'px';
+      rivendel.style.opacity = -2.7 + scroll * scrollOp;
+    } else {
+      shir.style.top = '';
+    }
+    if (scroll >= 3700) {
+      slideboxText[3].style.opacity = '0.85';
+    } else {
+      slideboxText[3].style.opacity = '';
+    }
 
-  if (scroll >= 1200) {
-    slideboxText.style.opacity = '1';
-    slideboxText.textContent = '3';
-  }
-  if (scroll >= 1400) {
-    slideboxText.style.opacity = '';
-    slideboxBg.style.cssText = "background: url('img/rivendel.jpg') no-repeat center;";
-  }
+    if (scroll >= 3800) {
+      rivendel.style.top = 0 - (scroll - 3800) + 'px';
+      karadras.style.opacity = -3.8 + scroll * scrollOp;
+    } else {
+      rivendel.style.top = '';
+    }
+    if (scroll >= 4800) {
+      slideboxText[4].style.opacity = '0.85';
+    } else {
+      slideboxText[4].style.opacity = '';
+    }
 
-  if (scroll >= 1500) {
-    slideboxText.style.opacity = '1';
-    slideboxText.textContent = '4';
-  }
-  if (scroll >= 1700) {
-    slideboxText.style.opacity = '';
-    slideboxBg.style.cssText = "background: url('img/karadras.jpg') no-repeat center;";
-  }
+    if (scroll >= 4900) {
+      karadras.style.top = 0 - (scroll - 4900) + 'px';
+      moria.style.opacity = -4.9 + scroll * scrollOp;
+    } else {
+      karadras.style.top = '';
+    }
+    if (scroll >= 5900) {
+      slideboxText[5].style.opacity = '0.85';
+    } else {
+      slideboxText[5].style.opacity = '';
+    }
 
-  if (scroll >= 1800) {
-    slideboxText.style.opacity = '1';
-    slideboxText.textContent = '5';
-  }
-  if (scroll >= 1900) {
-    slideboxText.style.opacity = '';
-    slideboxBg.style.cssText = "background: url('img/moria.jpg') no-repeat center;";
-  }
+    if (scroll >= 6000) {
+      moria.style.top = 0 - (scroll - 6000) + 'px';
+      lorien.style.opacity = -6 + scroll * scrollOp;
+    } else {
+      moria.style.top = '';
+    }
+    if (scroll >= 7000) {
+      slideboxText[6].style.opacity = '0.85';
+    } else {
+      slideboxText[6].style.opacity = '';
+    }
 
-  if (scroll >= 2000) {
-    slideboxText.style.opacity = '1';
-    slideboxText.textContent = '6';
-  }
-  if (scroll >= 2200) {
-    slideboxText.style.opacity = '';
-    slideboxBg.style.cssText = "background: url('img/lorien.jpg') no-repeat center;";
-  }
+    if (scroll >= 7100) {
+      lorien.style.top = 0 - (scroll - 7100) + 'px';
+      anduin.style.opacity = -7.1 + scroll * scrollOp;
+    } else {
+      lorien.style.top = '';
+    }
+    if (scroll >= 8100) {
+      slideboxText[7].style.opacity = '0.85';
+    } else {
+      slideboxText[7].style.opacity = '';
+    }
+  });
 
-  if (scroll >= 2300) {
-    slideboxText.style.opacity = '1';
-    slideboxText.textContent = '7';
-  }
-  if (scroll >= 2500) {
-    slideboxText.style.opacity = '';
-    slideboxBg.style.cssText = "background: url('img/anduin.jpg') no-repeat center;";
-  }
-
-  if (scroll >= 2600) {
-    slideboxText.style.opacity = '1';
-    slideboxText.textContent = '8';
-  }
+  console.log(document.documentElement.scrollTop);
 });
-
-console.log(document.documentElement.scrollTop);
